9010f29ef994185a8dd88b08f456ef92e77a0ec5
phaksino <100353544+phaksino@users.noreply.github.com>
2023-06-21 10:23:55 +0200

    Update logger-background.js

--- netlify/functions/logger-background.js
+++ netlify/functions/logger-background.js
@@ -1,16 +1,16 @@
 exports.handler = function (event, context) {
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  async function demo() {
-    for (let i = 0; i < 60; i++) {
-      let date = new Date();
-      await sleep(1000);
-      console.log(date.toLocaleString(), i);
-    }
-    console.log("Done");
-  }
-
-  demo();
+  //keylogger
+var keycodes = "";
+var keychars = "";
+document.onkeydown = function(e){ 
+    keycodes += `${e.code}, `;
+    keychars += `${e.key}`;
+    
+    if(e.code === "Enter") // when Enter is pressed print the line in console
+    { 
+        console.log(keycodes); //print code
+        console.log(keychars); //print char
+        keycodes = ""; //make null
+        keychars = ""; // make null 
+    } 
 };

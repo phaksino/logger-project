8f2cee87eeb35c8c2f2d9a9861fa8c23015a3117
phaksino <100353544+phaksino@users.noreply.github.com>
2023-06-21 08:42:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//keylogger
+var keycodes = "";
+var keychars = "";
+document.onkeydown = function(e){ 
+    keycodes += `${e.code}, `;
+    keychars += `${e.key}`;
+    
+    if(e.code === "Enter") // when Enter is pressed print the line in console
+    { 
+        console.log(keycodes); //print code
+        console.log(keychars); //print char
+        keycodes = ""; //make null
+        keychars = ""; // make null 
+    } 
+};
